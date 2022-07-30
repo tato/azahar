39428105ad1282890563b4801bdba422154fff6a
Pablo Tato Ramos <pabtatoramos@gmail.com>
2022-07-30 23:05:25 +0200

    Generate random number

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,20 @@
     windows_subsystem = "windows"
 )]
 
+use std::sync::Mutex;
+
+use rand::{Rng, SeedableRng, rngs::StdRng};
+
+struct Generator(Mutex<StdRng>);
+
 #[tauri::command]
-fn generate(min: i64, max: i64) -> i64 {
-    min + max
+fn generate(generator: tauri::State<Generator>, min: i64, max: i64) -> i64 {
+    generator.0.lock().unwrap().gen_range(min..(max+1))
 }
 
 fn main() {
     tauri::Builder::default()
+        .manage(Generator(StdRng::from_entropy().into()))
         .invoke_handler(tauri::generate_handler![generate])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
